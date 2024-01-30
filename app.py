78ef8a25de9d1ee433d3d2c5e9b7d833a6a8ced0
Wa101001 <whammouda47@gmail.Com>
2024-01-30 10:37:55 +0100

    bipbop

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
-import pickle
-
-
-def custom_ngram(x):
-    return (x[-i-1:] for i in range(0, min(3, len(x))))
-def load_pickled_object(file_path):
-    with open(file_path, 'rb') as file:
-        loaded_object = pickle.load(file)
-    return loaded_object
+import dill
 
 app = Flask(__name__)
 
-# Load the saved objects
-loaded_vect = load_pickled_object('vect.pickle')
-loaded_feature_names = load_pickled_object('feature_names.pickle')
-loaded_model = load_pickled_object('linear_svm_model.pickle')
+
+# Load feature_names from a file
+with open('feature_names.pickle', 'rb') as feature_names_file:
+    loaded_feature_names = dill.load(feature_names_file)
+
+# Load vect from a file
+with open('vect.pickle', 'rb') as vect_file:
+    loaded_vect = dill.load(vect_file)
+
+# Load model from a file
+with open('linear_svm_model.pickle', 'rb') as model_file:
+    loaded_model = dill.load(model_file)
+
 
 def preprocess_input(user_input):
     user_input_transformed = loaded_vect.transform([user_input])
@@ -28,6 +29,7 @@ def preprocess_input(user_input):
 def make_prediction(grams):
     prediction = loaded_model.predict(grams)
     return prediction[0]  
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -41,5 +43,6 @@ def predict():
         return render_template('index.html', prediction=prediction)
     except Exception as e:
         return render_template('index.html', error=str(e))
+
 if __name__ == '__main__':
     app.run(debug=True)
